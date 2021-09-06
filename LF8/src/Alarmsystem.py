e3b408e9650ed56e2f17868ddf6eb70bcbefb3f9
Mieke Aljets <maljets@web.de>
2021-09-06 22:32:12 +0200

    updates Alarmsystem

--- LF8/src/Alarmsystem.py
+++ LF8/src/Alarmsystem.py
@@ -12,19 +12,18 @@ class Alarmsystem:
             logging: logging
     ):
         if parameter_value < soft_limit and logging_level == 'debug':
-            # ToDo
             log_msg = parameter_label + ' = ' + str(parameter_value)
             Alarmsystem.log('debug', log_msg, logging)
             return
 
         if parameter_value >= soft_limit:
             if parameter_value >= hard_limit:
-                log_msg = parameter_label.upper() + ' EXCEEDS HARD LIMIT OF ' + str(parameter_value) + ': ' \
+                log_msg = parameter_label.upper() + ' EXCEEDS HARD LIMIT OF ' + str(hard_limit) + ': ' \
                           + parameter_label + ' = ' + str(parameter_value)
                 Alarmsystem.log('warning', log_msg, logging)
                 # ToDo Email versenden
                 return
-            log_msg = parameter_label.upper() + ' EXCEEDS SOFT LIMIT OF ' + str(parameter_value) + ': ' \
+            log_msg = parameter_label.upper() + ' EXCEEDS SOFT LIMIT OF ' + str(soft_limit) + ': ' \
                       + parameter_label + ' = ' + str(parameter_value)
             Alarmsystem.log('warning', log_msg, logging)
             return

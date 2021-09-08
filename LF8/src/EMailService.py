3ea09fb71f89dd7a19764c0a916c440c65e787f9
niemcu-py <ben.zimmerhh@gmail.com>
2021-09-08 09:56:07 +0200

    EMailDienst Changes

--- LF8/src/EMailService.py
+++ LF8/src/EMailService.py
@@ -1,22 +1,36 @@
 import smtplib
 
-
 class EMailDienst():
     def __init__(self, host, adress, password):
         self.host = host
         self.adress = adress
         self.password = password
-
+    
     def sendEMail(self, receiver, message):
         try:
-            smtpObj = smtplib.SMTP_SSL(self.host, 465)
-            smtpObj.ehlo()
+            smtpObj = smtplib.SMTP(self.host, 587)
+            smtpObj.starttls(keyfile=None, certfile=None, context=None)
+            #smtpObj.ehlo()
             smtpObj.login(self.adress, self.password)
             smtpObj.sendmail(self.adress, receiver, message)
             print("Success")
         except smtplib.SMTPException:
             print("Failure")
+            
+    def buildBody(self, percent):  
+        body = """Warning \n\nThe harddrive of the system is %d%% filled.""" % (percent)
+        return(body)
+    
+    def buildMessage(self, sent_from, to, subject, body):        
+        
+        message = """From: %s\nTo: %s\nSubject: %s\n\n%s""" % (sent_from, ", ".join(to), subject, body)
+        
+        return(message)
 
 
-client = EMailDienst('smtp.gmail.com', 'lf8warningtool@gmail.com', 'LF8!Gruppe4')
-client.sendEMail('ben.zimmerhh@gmail.com', 'hallo')
+client = EMailDienst('smtp-mail.outlook.com', 'lf8warningtool@outlook.de', 'LF8!Gruppe4')
+body = client.buildBody(99)
+print(body)
+message = client.buildMessage('lf8warningtool@outlook.de', ['ben.zimmerhh@gmail.com'], 'Warning', body)
+print(message)
+client.sendEMail('ben.zimmerhh@gmail.com', message)
